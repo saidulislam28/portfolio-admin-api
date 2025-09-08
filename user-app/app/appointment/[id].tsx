8e194ea03ad3adaee5bb3bbd754b57b847ee7aa3
saidulislam28 <saidulislams9028@gmail.com>
2025-09-08 12:30:10 +0600

    add base input field

--- user-app/app/appointment/[id].tsx
+++ user-app/app/appointment/[id].tsx
@@ -377,7 +377,7 @@ const AppointmentDetailPage = () => {
           <BaseButton
             title=" Feedback Report"
             onPress={handleFeedback}
-            isLoading={isDisabled}
+            disabled={isDisabled}
             variant="primary"
             fullWidth={false}
           />
