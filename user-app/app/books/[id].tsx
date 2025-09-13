7e0deace9cdba09bfc5d87a7b2a7ceb1831a407f
coder-mehedi-hasan <mh8399807@gmail.com>
2025-09-13 16:41:10 +0600

    fixed book details design

--- user-app/app/books/[id].tsx
+++ user-app/app/books/[id].tsx
@@ -125,7 +125,9 @@ export default function BookDetailsScreen() {
             </View>
           ) : (
 
-            <BaseButton title="Add to Cart" onPress={handleAddToCart} isLoading={cartLoading} />
+            <View style={{paddingBottom: 150}}>
+              <BaseButton title="Add to Cart" onPress={handleAddToCart} isLoading={cartLoading} />
+            </View>
           )}
         </View>
         {/* Cart Summary - Show only if items in cart */}
